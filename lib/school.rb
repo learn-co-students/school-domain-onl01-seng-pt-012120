834d02187acffea6d4e8c5ac94a1e2f6ae45cc7d
Roberto Livi <robertomlivi@outlook.com>
2020-01-31 01:18:16 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  
+  attr_accessor :name, :add_student, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    self.roster.each do |grade, students|
+      students.sort!
+    end
+  end
+end
