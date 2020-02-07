7447f75be6ae5876db29f132652996d8f9e68033
Norberto Santiago <norbertosantiago82@gmail.com>
2020-02-07 03:58:58 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School 
+  attr_accessor :student_name, :grade 
+  attr_reader :schoolname, :roster, :student_name, :grade  
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {} 
+  end
+  
+  def add_student(student_name, grade)
+    if @roster[grade]
+       @roster[grade] << student_name
+  else
+    @roster[grade] = []
+    @roster[grade] << student_name
+  end #this code will make the program able to add both student name and grade 
+  end 
+  
+  def grade(grade)
+    @roster[grade] #retrieve students from an specific grade 
+  end 
+  
+  def sort
+    @roster.each do |grade, student_name|
+      @roster[grade] = student_name.sort 
+    end 
+  end 
+end 
