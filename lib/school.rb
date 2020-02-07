2bfc190813f6ca001410e37ea6526467f53da50e
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-02-07 21:03:47 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = { }
+  end
+end
+
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def roster=(roster)
+    @roster = roster
+  end
+  
+  def roster
+    @roster
+  end
+  
