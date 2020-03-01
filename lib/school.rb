84c299ed0165b7302d6f88b0835218bb360d69f9
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-01 00:40:05 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School
+  
+  # attr_accessor :roster, :student_name, :school_name
+  # is this relevant? still passes the test without..
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = Hash.new { |h, k| h[k] = [] }
+    # @roster = Hash.new { |name, grade| name[grade] = [] }
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(name, grade)
+    @roster[grade] << name
+  end
+  
+  def grade(number)
+    @roster[number]
+  end
+  
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
+  
+  
+end
